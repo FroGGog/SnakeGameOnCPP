1cfce047f2f0ec4d5a1183ca3ad6ec52b3363da3
Danya Bucenin <danyabucenin@gmail.com>
2024-01-03 23:24:53 +0300

    Snake game completed

--- SnakeGame/Src/main.cpp
+++ SnakeGame/Src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#include <conio.h>
+#include <conio.h> // for user inputs 
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
 enum class Dirs { Left = 1, Right, Up, Down };
 
-const double FPS{ 0.005 }; //updates per period of time
+const double FPS{ 0.003 }; //updates per period of time
 const int FIELDSIZE{ 100 };//all gamefield size
 const int ROWCUT{ 20 };//get field into new line. = FIELDSIZE / N 
 
 
-//main snake head class
 class Head {
 
 public:
@@ -25,37 +26,31 @@ public:
     short lenght{ 1 };
 
     void Move(short dir) {
-        if (direction_x + dir >= ::FIELDSIZE) {
-            if (this->dir == Dirs::Right) {
-                direction_x = 0;
-                saved_x = ::FIELDSIZE - 1;
-                return;
-            }
-            else if (this->dir == Dirs::Down) {
+        if (direction_x + dir >= ::FIELDSIZE) {//check if player if outside field and going down
+            if (this->dir == Dirs::Down) {
                 saved_x = direction_x;
-                direction_x = direction_x % 10;
-                return;
-            }
-            else {
+                direction_x -= ((::FIELDSIZE / ROWCUT) - 1) * ROWCUT;
                 return;
             }
         }
-        else if (direction_x + dir < 0) {
-            if (this->dir == Dirs::Left) {
-                direction_x = ::FIELDSIZE - 1;
-                saved_x = 0;
-                return;
-            }
-            else if (this->dir == Dirs::Up) {
+        else if (direction_x + dir < 0) { // and going up
+            if (this->dir == Dirs::Up) {
                 saved_x = direction_x;
                 direction_x += ( ( ::FIELDSIZE / ROWCUT) - 1 ) * ROWCUT;
                 return;
             }
-            else {
-                return;
-            }
         }
-        else {
+        if ((direction_x + 1) % ::ROWCUT == 0 && this->dir == Dirs::Right) {//going right
+            saved_x = direction_x;
+            direction_x -= ::ROWCUT-1;
+            return;
+        }
+        else if (direction_x % ::ROWCUT == 0 && this->dir == Dirs::Left) { // going left
+            saved_x = direction_x;
+            direction_x += ::ROWCUT - 1;
+            return;
+        }
+        else {//if player is on field he goes toward dir
             saved_x = direction_x;
             direction_x += dir;
         }
@@ -66,7 +61,7 @@ public:
         this->lenght = static_cast<short>(BodySize);
     }
 
-    bool EatFruit(std::vector<char>& gameField) {
+    bool EatFruit(std::vector<char>& gameField) {//check if player's head is on fruit
         if (gameField[this->direction_x] == '@') {
             return true;
         }
@@ -74,10 +69,8 @@ public:
             return false;
         }
     }
-
-
-
 };
+
 
 struct Body {
 
@@ -86,15 +79,15 @@ public:
     short saved_x{0};
     char bodyChar{ 'O' };
 
-    Body(Head &head) {
+    Body(Head &head) {//we need to get last head position for first body part
         direction_x = head.saved_x;
 
     }
-    Body(short _dir_x, short s_x) {
+    Body(short _dir_x, short s_x) {//and for other body parts get forward body position
         this->direction_x = _dir_x;
         this->saved_x = s_x;
     }
-    void UpdatePos(Head &head, std::vector<char> &gameField) {
+    void UpdatePos(Head &head, std::vector<char> &gameField) {//update position for first body part
         saved_x = direction_x;
         direction_x = head.saved_x;
         gameField[direction_x] = bodyChar;
@@ -104,7 +97,7 @@ public:
         
         
     }
-    void UpdatePos(short saved_x, std::vector<char>& gameField) {
+    void UpdatePos(short saved_x, std::vector<char>& gameField) {//update position for other body parts
         this->saved_x = direction_x;
         direction_x = saved_x;
         gameField[direction_x] = bodyChar;
@@ -114,16 +107,16 @@ public:
 };
 
 
-
-
 struct Fruit{
 
 public:
     int pos{25};
     char fruitChar{ '@' };
+    short fruit_count{ 0 };
+    short fruit_pos{ 0 };
 
     void SpawnFruit() {
-        return;
+        fruit_pos = rand() % (::FIELDSIZE - 1);
     };
 
 };
@@ -139,18 +132,15 @@ void printGameField(std::vector<char> gameField){
 	}
 }
 
+
 bool Input(Head &player)
 {
-    /*if (player.direction_x > 6) {
-        player.dir = Dirs::Down;
-        return false;
-    }*/
-    if (_kbhit())
+    if (_kbhit())//check if player pressed any buttons 
     {
         switch (_getch())
         {
         case 'a':         
-            if (player.dir == Dirs::Right) {
+            if (player.dir == Dirs::Right) {//prevent player from going opposite direction immediately
                 return false;
             }
             player.dir = Dirs::Left;
@@ -183,7 +173,8 @@ bool Input(Head &player)
     
 }
 
-bool Update(std::chrono::system_clock::time_point tp) {
+
+bool Update(std::chrono::system_clock::time_point tp) {//check if programm must update game screen
     auto time_check = std::chrono::system_clock::now();
     std::chrono::duration<float> timeToUPD{ (time_check - tp) / 60 };
     if (timeToUPD.count() > ::FPS) {
@@ -196,29 +187,37 @@ bool Update(std::chrono::system_clock::time_point tp) {
 void AddTail(std::vector<Body>& SnakeBody) {
     short a = SnakeBody[SnakeBody.size() - 1].direction_x - 1;
     short b = SnakeBody[SnakeBody.size() - 1].saved_x - 1;
-    Body new_tail{ a, b };
+    Body new_tail{ a, b };//add new tail part which get forward tail part saved_x position
     SnakeBody.push_back(new_tail);
     return;
 }
 
 
+void GameOver(short snakeLenght, std::chrono::system_clock::time_point tp) {
+    system("cls");
+    auto now = std::chrono::system_clock::now();
+    std::chrono::duration<float> timeForScore{ (now - tp) / 60 };
+    std::cout << "You lose!\nYou eat : " << snakeLenght << " fruits!\n";
+    std::cout << "Your total score is : " << (int)(timeForScore.count() + snakeLenght * 20);
+}
+
+
 int main() {
-
+    srand(time(NULL));//get seed for random num
     auto time_start = std::chrono::system_clock::now();
+    auto score_timer = time_start;//time for score
+    bool inGame{ true };
 
-    Head player{};
-    Body part1{ player };
+    Head player{};//player head object
+    Body part1{ player }; //firt added body part
     Fruit fruit{};
-    Fruit fruit2{};
 
     std::vector<Body> SnakeBody{part1};
-
 	std::vector<char> gameField(::FIELDSIZE, '*');
-    gameField[0] = player.playerChar;
-    gameField[4] = fruit.fruitChar;
-    gameField[25] = fruit2.fruitChar;
 
-    while (true) {
+    gameField[0] = player.playerChar;//start player pos
+
+    while (inGame) {
         
         if (Update(time_start)) {
             //player.SetLenght(SnakeBody.size());
@@ -239,14 +238,25 @@ int main() {
                 default:
                     break;
                 }
-                if (player.EatFruit(gameField)) {
+                if (gameField[player.direction_x] == 'O') {
+                    inGame = false;
+                    continue;
+                }
+                if (player.EatFruit(gameField)) {//if player eat fruit add one tail, delte fruit 
+                    fruit.fruit_count--;
                     AddTail(SnakeBody);
                     player.SetLenght(SnakeBody.size());
                 }
-                gameField[player.direction_x] = player.playerChar;
-                if (player.lenght == 0) {
-                    gameField[player.saved_x] = '*';
+                if (fruit.fruit_count == 0) {//if there are no fruits on field spawn new one 
+                    fruit.SpawnFruit();
+                    while(gameField[fruit.fruit_pos] != '*') {//prevent fruit from spawning on player tail\head
+                        fruit.SpawnFruit();
+                    }
+                    gameField[fruit.fruit_pos] = fruit.fruitChar;
+                    fruit.fruit_count++;
+                    
                 }
+                gameField[player.direction_x] = player.playerChar;
                 for (auto i{ 0 }; i < SnakeBody.size(); i++) {
                     if (i == 0) {
                         SnakeBody[i].UpdatePos(player, gameField);
@@ -265,7 +275,7 @@ int main() {
         
     }
 	
-
+    GameOver((player.lenght - 1), score_timer);
 
 	return 0;
 }
